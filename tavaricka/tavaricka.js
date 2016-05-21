3c1b8564203e5b46459264d137cb7c27912bcd6c
fansek <fgrega@gmail.com>
2016-05-21 23:37:29 +0200

    tavaricka sub-site added

---
+++ tavaricka/tavaricka.js
@@ -0,0 +1,142 @@
+// function createAttribute(name, value) {
+//    const result = document.createAttribute(name);
+//    result.value = value;
+//    return result;
+// }
+
+// function createElement(elementName, attributes) {
+//    const element = document.createElement(elementName);
+//    for (var name in attributes) {
+//       if (attributes.hasOwnProperty(name)) {
+//          element.setAttributeNode(createAttribute(name, attributes[name]));
+//       }
+//    }
+//    return element;
+// }
+const canvas = document.getElementById('canvas');
+
+function createElementSvg(elementName, attributes) {
+   const element = document.createElementNS('http://www.w3.org/2000/svg', elementName);
+   for (var name in attributes) {
+      if (attributes.hasOwnProperty(name)) {
+         element.setAttribute(name, attributes[name]);
+      }
+   }
+   return element;
+}
+
+function mapToDistance(value) {
+   return value * 50;
+}
+
+function mapToPosition(value) {
+   return mapToDistance(value) + 50;
+}
+
+function putTree(x, y, r, name) {
+   x = mapToPosition(x);
+   y = mapToPosition(y);
+   r = mapToDistance(r);
+   canvas.appendChild(
+      createElementSvg('circle', { cx: x, cy: y, r, stroke: 'green', 'stroke-width': r, fill: 'brown' }));
+   if (name !== undefined) {
+      var text = createElementSvg('text', { x: x + (1.5 * r), y: y + (2 * r), 'font-size': 2 * r });
+      text.appendChild(document.createTextNode(name));
+      canvas.appendChild(text);
+   }
+}
+
+function putLabeledLine(x1, y1, d, vertical, label) {
+   x1 = mapToPosition(x1);
+   y1 = mapToPosition(y1);
+   d = mapToDistance(d);
+   const x2 = vertical ? x1 : x1 - d;
+   const y2 = vertical ? y1 - d : y1;
+
+   canvas.appendChild(
+      createElementSvg('line', { x1, y1, x2, y2, stroke: 'black', 'stroke-width': 1 }));
+
+   if (label !== undefined) {
+      var text;
+      if (vertical) {
+         text = createElementSvg('text', { x: x1 + 5, y: (y1 + y2) / 2, 'font-size': 10 });
+      } else {
+         text = createElementSvg('text', { x: (x1 + x2) / 2, y: y1 + 12, 'font-size': 10, 'text-anchor': 'middle' });
+      }
+      text.appendChild(document.createTextNode(label));
+      canvas.appendChild(text);
+   }
+}
+
+///////
+
+function toPositions(distances) {
+   const positions = {};
+   for (var name in distances) {
+      if (distances.hasOwnProperty(name)) {
+         const value = distances[name];
+         computePosition(name, value, 'x', distances, positions);
+         computePosition(name, value, 'y', distances, positions);
+      }
+   }
+   return positions;
+}
+
+function computePosition(name, value, axis, distances, positions) {
+   const otherName = value['d' + axis];
+   if (positions[name] !== undefined && positions[name][axis] !== undefined) {
+      return positions[name][axis];
+   }
+   if (otherName === undefined) {
+      putPosition(positions, name, axis, value[axis]);
+      return value[axis];
+   } else {
+      const otherValue = distances[otherName];
+      if (otherValue !== undefined) {
+         const otherPosition = computePosition(otherName, otherValue, axis, distances, positions);
+         if (otherPosition !== undefined) {
+            const result = value[axis] + otherPosition;
+            putPosition(positions, name, axis, result);
+            return result;
+         }
+      }
+      return undefined;
+   }
+}
+
+function putPosition(positions, name, axis, value) {
+   if (positions[name] === undefined) {
+      positions[name] = {};
+   }
+   positions[name][axis] = value;
+   return positions;
+}
+
+var distances = {
+   c1: { x: 0, y: 0 },
+   c2: { x: 17, y: 0, dx: 'c1', dy: 'c1' },
+   c3: { x: 0, y: 55, dx: 'c1', dy: 'c1' },
+   c4: { x: 17, y: 0, dx: 'c3', dy: 'c3' },
+   t1: { x: -10, y: -5, dx: 'c5', dy: 'c6' },
+   t2: { x: -10, y: -5, dx: 'c2', dy: 'c3' }
+};
+
+const positions = toPositions(distances);
+
+for (var name in positions) {
+   if (positions.hasOwnProperty(name)) {
+      const x = positions[name].x;
+      const y = positions[name].y;
+
+      const dx = distances[name].x;
+      const dy = distances[name].y;
+
+      putTree(x, y, 0.2, name);
+      if (distances[name].dx !== undefined && x !== 0) {
+         putLabeledLine(x, y, dx, false, dx);
+      }
+      if (distances[name].dy !== undefined && y !== 0) {
+         putLabeledLine(x, y, dy, true, dy);
+      }
+   }
+}
